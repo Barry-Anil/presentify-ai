859a2602e6acd0c610a63515614a9b47c21f213b
Barry Anil <barryanil1@gmail.com>
2025-04-02 15:35:05 +0530

    Fix : prisma singleton issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,11 @@
 import {PrismaClient} from '@prisma/client'
 
+// Add this to tell ESLint to ignore the next declaration
+/* eslint-disable no-var */
 declare global {
-    var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined
 }
+/* eslint-enable no-var */
 
 export const client = globalThis.prisma || new PrismaClient()
 if(process.env.NODE_ENV !== 'production') globalThis.prisma = client
